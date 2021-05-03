f2de7ec71d7c052168aa72211ac703d7309673d9
BigRodri <rodrigonnes@icloud.com>
2021-05-03 19:33:43 -0300

    Flock.IA
    Atualização 02/05/2021 19:33

---
+++ Flocking/Assets/Flock.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flock : MonoBehaviour
+{
+    public FlockManager myManager;
+    float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {   //Configuração de velocidade maxima e minima do cardume
+        speed = Random.Range(myManager.minSpeed, myManager.maxSpeed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {    //Degine a velocidade do cardume
+        transform.Translate(0, 0, Time.deltaTime * speed);
+    }
+}
+
---
+++ Flocking/Assets/FlockManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlockManager : MonoBehaviour
+{
+    //Cria slots para guardar as informações
+    public GameObject fishPrefab;
+    public int numFish= 20;
+    public GameObject[] allFish; 
+    public Vector3 swinLimits = new Vector3(5, 5, 5);
+
+      //Menu para limitar a velocidade 
+    [Header("Configurações do Cardume")]
+    [Range(0.0f, 5.0f)]
+    public float minSpeed;
+    [Range(0.0f, 5.0f)]
+    public float maxSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {    //Coloca os peixes na lista
+        allFish = new GameObject[numFish];
+        for (int i = 0; i < numFish; i++)
+        {//Fazer os peixes nadarem em uma direção 
+            Vector3 pos = this.transform.position + new Vector3(Random.Range(-swinLimits.x, swinLimits.x), 
+                                                                Random.Range(-swinLimits.y, swinLimits.y), 
+                                                                Random.Range(-swinLimits.z, swinLimits.z));
+            allFish[i] = (GameObject)Instantiate(fishPrefab, pos, Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
